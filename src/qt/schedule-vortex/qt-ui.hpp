b81b270cc35effdc7ca0713b67f2a5316d36afbc
Ryan Smith <rysmith2113@gmail.com>
2023-02-09 11:15:10 -0500

    Add a central place to access Ui::MainWindow

---
+++ src/qt/schedule-vortex/qt-ui.hpp
@@ -0,0 +1,17 @@
+//
+// FILENAME: qt-ui.hpp | Schedule Vortex
+// DESCRIPTION: Store the Qt GUI elements loaded from XML
+// CREATED: 2023-02-05 @ 5:46 PM
+// COPYRIGHT: Copyright (c) 2023 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#ifndef QT_UI_HPP
+#define QT_UI_HPP
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+#endif // QT_UI_HPP
