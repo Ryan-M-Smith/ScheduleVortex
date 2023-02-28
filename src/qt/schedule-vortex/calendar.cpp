a804b2225597e676f5281c68a8fb40d533a777a5
Ryan Smith <rysmith2113@gmail.com>
2023-02-28 11:34:57 -0500

    Add a way to search for data within the calendar

--- src/qt/schedule-vortex/calendar.cpp
+++ src/qt/schedule-vortex/calendar.cpp
@@ -12,19 +12,47 @@ Calendar::Calendar(QCalendarWidget* calendar):
 {}
 
 Calendar::~Calendar() {
-	delete m_Calendar;
-
 	for (auto date: m_SignificantDates) {
 		delete date;
 	}
 	m_SignificantDates.clear();
 }
 
+// Add an event to the calendar
 void Calendar::AddEvent(int day, int month, int year, string eventName) {
 	Date* eventDate = new Date(day, month, year, eventName);
-	m_SignificantDates.push_back(eventDate);
+	Date* calendarDate = GetDate(eventDate); // Any pre-existing date info on the given date
 
-	for (auto date: m_SignificantDates) {
-		qDebug() << date->m_Events.front();
+	//
+	// If an event is being added to a date that already has events on it, the
+	// new event can just be added to the correct Date object within the calendar.
+	// New dates have to be added first, but the new event will be included with the
+	// date.
+	//
+	if (!Contains(eventDate)) {
+		m_SignificantDates.push_back(eventDate);
 	}
+	else if (calendarDate != nullptr) {
+		calendarDate->AddEvent(eventName);
+	}
+
+	emit AddedEvent(Date::ToQDate(*eventDate)); // Notify the application that an event was added
+}
+
+// Access calendar dates using a generic date (month, day, and year)
+Date* Calendar::GetDate(Date* date) {
+	auto result = std::find_if(m_SignificantDates.begin(), m_SignificantDates.end(), [date](Date* currDate) -> bool {
+		return *currDate == *date;
+	});
+
+	return (result != m_SignificantDates.end())? *result : nullptr;
+}
+
+// Determine if the calendar contains event data on a certain date
+bool Calendar::Contains(Date* date) {
+	auto result = std::find_if(m_SignificantDates.begin(), m_SignificantDates.end(), [date](Date* currDate) -> bool {
+		return *currDate == *date;
+	});
+
+	return result != m_SignificantDates.end();
 }
--- src/qt/schedule-vortex/calendar.hpp
+++ src/qt/schedule-vortex/calendar.hpp
@@ -5,25 +5,39 @@
 // COPYRIGHT: Copyright (c) 2023 by Ryan Smith <rysmith2113@gmail.com>
 //
 
+#ifndef CALENDAR_HPP
+#define CALENDAR_HPP
+
 #include <vector>
 #include <string>
+#include <algorithm>
 
+#include <QObject>
 #include <QCalendarWidget>
+#include <QTextCharFormat>
+#include <QDate>
 
 #include "date.hpp"
 
 using std::string;
 
-class Calendar {
+class Calendar : public QObject {
+	Q_OBJECT
+
 public:
 	Calendar(QCalendarWidget* calendar);
 	~Calendar();
 
 	void AddEvent(int day, int month, int year, string eventName);
+	Date* GetDate(Date* date);
+	bool Contains(Date* date);
+
+signals:
+	void AddedEvent(const QDate& date);
 
 private:
 	QCalendarWidget* m_Calendar;
-
 	std::vector<Date*> m_SignificantDates;
-
 };
+
+#endif // CALENDAR_HPP
